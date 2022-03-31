2b7752749fd1d8ecc26901ee23a57a498b04d1c0
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2022-03-31 23:39:11 +0200

    Add vcxproj project type id

--- SimpleSolution.Core/SupportedTypes.cs
+++ SimpleSolution.Core/SupportedTypes.cs
@@ -3,15 +3,13 @@ namespace SimpleSolution.Core;
 public static class SupportedTypes
 {
     private const string SolutionDirectoryTypeId = "2150E333-8FDC-42A3-9474-1A3956D46DE8";
-    private const string CSharpProjectTypeId = "FAE04EC0-301F-11D3-BF4B-00C04F79EFBC";
-    private const string FSharpProjectTypeId = "F2A71F9B-5D33-465A-A702-920D77279786";
-    private const string VbNetProjectTypeId = "F184B08F-C81C-45F6-A57F-5ABD9991F28F";
 
     private static readonly Dictionary<string, string> ProjectTypes = new()
     {
-        {".csproj", CSharpProjectTypeId},
-        {".fsproj", FSharpProjectTypeId},
-        {".vbproj", VbNetProjectTypeId}
+        {".csproj", "2150E333-8FDC-42A3-9474-1A3956D46DE8"},
+        {".fsproj", "F2A71F9B-5D33-465A-A702-920D77279786"},
+        {".vbproj", "FAE04EC0-301F-11D3-BF4B-00C04F79EFBC"},
+        {".vcxproj", "8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942"}
     };
     
     public static bool HasProjectExtension(string fileName)
